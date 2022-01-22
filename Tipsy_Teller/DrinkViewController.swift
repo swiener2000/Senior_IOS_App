97daf76fa10e6358f915bb817ad56d8052b45ab0
Stephanie Wiener <54724492+swiener2000@users.noreply.github.com>
2022-01-22 10:39:06 -0500

    Added another view controller

---
+++ Tipsy_Teller/DrinkViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DrinkViewController.swift
+//  Tipsy_Teller
+//
+//  Created by Stephanie Wiener on 1/22/22.
+//
+
+import UIKit
+
+class DrinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
