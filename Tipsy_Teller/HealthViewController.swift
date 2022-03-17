f194d021772af03135f4467072133eecfdf34f71
Stephanie Wiener <54724492+swiener2000@users.noreply.github.com>
2022-03-17 17:07:02 -0400

    Added highlight color to effects

--- Tipsy_Teller/HealthViewController.swift
+++ Tipsy_Teller/HealthViewController.swift
@@ -10,13 +10,35 @@ import Parse
 class HealthViewController: UIViewController{
     
 
+    
+    @IBOutlet weak var one: UITextView!
+    @IBOutlet weak var two: UITextView!
+    @IBOutlet weak var three: UITextView!
+    @IBOutlet weak var four: UITextView!
+    @IBOutlet weak var five: UITextView!
+    @IBOutlet weak var six: UITextView!
+    @IBOutlet weak var seven: UITextView!
+    @IBOutlet weak var eight: UITextView!
+    @IBOutlet weak var nine: UITextView!
+    
+    @IBOutlet weak var oneE: UITextView!
+    @IBOutlet weak var twoE: UITextView!
+    @IBOutlet weak var threeE: UITextView!
+    @IBOutlet weak var fourE: UITextView!
+    @IBOutlet weak var fiveE: UITextView!
+    @IBOutlet weak var sixE: UITextView!
+    @IBOutlet weak var sevenE: UITextView!
+    @IBOutlet weak var eightE: UITextView!
+    @IBOutlet weak var nineE: UITextView!
+    
+    
     var BAC: Double = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Loaded")
         // Do any additional setup after loading the view.
         BAC = getBAC()
-        
+        highlightEffect()
     }
     
     func getBAC() -> Double {
@@ -40,6 +62,37 @@ class HealthViewController: UIViewController{
         bac = BACArray.last!
         print(bac)
         return bac
+    }
+    
+    func highlightEffect() {
+        if BAC >= 0.01 && BAC <= 0.03 {
+            one.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            oneE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.04 && BAC <= 0.06 {
+            two.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            twoE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.07 && BAC <= 0.09 {
+            three.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            threeE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.10 && BAC <= 0.12 {
+            four.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            fourE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.13 && BAC <= 0.15 {
+            five.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            fiveE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.16 && BAC <= 0.20 {
+            six.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            sixE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.25 && BAC <= 0.30 {
+            seven.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            sevenE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.35 && BAC <= 0.40 {
+            eight.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            eightE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        } else if BAC >= 0.40 {
+            nine.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+            nineE.backgroundColor = UIColor(red: 39/255, green: 53/255, blue: 182/255, alpha: 1)
+        }
     }
     /*let username = PFUser.current()?.username
     
